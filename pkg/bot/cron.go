906ed0580d14ae464c7adb2a7451b8c7c97fc83d
Taisuke Miyazaki <imishinist@gmail.com>
2021-03-03 00:13:08 +0900

    add cron setting

--- pkg/bot/cron.go
+++ pkg/bot/cron.go
@@ -1,6 +1,7 @@
 package bot
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -13,16 +14,19 @@ var (
 func fromCron(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if header := r.Header.Get(appEngineCronHeader); header != "true" {
+			log.Println("header check failed")
 			w.WriteHeader(http.StatusNotAcceptable)
 			w.Write([]byte("header check failed"))
 			return
 		}
 		host := echo.ExtractIPDirect()(r)
-		if host != "10.0.0.1" || host != "0.1.0.1" {
-			w.WriteHeader(http.StatusNotAcceptable)
-			w.Write([]byte("ip check failed"))
-			return
-		}
+		log.Println(host)
+		// if host != "10.0.0.1" && host != "0.1.0.1" {
+		// 	log.Println("ip check failed")
+		// 	w.WriteHeader(http.StatusNotAcceptable)
+		// 	w.Write([]byte("ip check failed"))
+		// 	return
+		// }
 		next.ServeHTTP(w, r)
 	})
 }
