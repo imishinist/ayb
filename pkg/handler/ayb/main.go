9d6e17939124db95e06eb8810412c527f530df8c
Taisuke Miyazaki <imishinist@gmail.com>
2021-03-02 23:28:16 +0900

    add gae setting file

--- pkg/handler/ayb/main.go
+++ pkg/handler/ayb/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/imishinist/ayb/pkg/bot"
 )
 
 func main() {
 	s := bot.CreateServer()
-	log.Fatal(s.ListenAndServe(":8090"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8090"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(s.ListenAndServe(fmt.Sprintf(":%s", port)))
 }
